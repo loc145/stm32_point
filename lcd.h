b915010b01675537d1819ebfdd08acb055693435
loczing9c <73115062+loczing9c@users.noreply.github.com>
2020-12-18 03:42:21 +0700

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -9,8 +9,8 @@
 #define INC_LCD_H_
 #include "stm32f0xx_hal.h"
 
-// lcd_RS 	GPIOA	GPIO_PIN_9		//PA10
-// lcd_E 	GPIOA	GPIO_PIN_10		//PA9
+// lcd_RS 	GPIOA	GPIO_PIN_9		//PA9
+// lcd_E 	  GPIOA	GPIO_PIN_10		//PA10
 // lcd_D4 	GPIOA	GPIO_PIN_5		//PA5
 // lcd_D5 	GPIOA	GPIO_PIN_6		//PA6
 // lcd_D6 	GPIOA	GPIO_PIN_7		//PA7
@@ -23,8 +23,8 @@
 #define port_lcd_D6 	GPIOA
 #define port_lcd_D7 	GPIOB
 
-#define pin_lcd_RS		GPIO_PIN_10
-#define pin_lcd_E		GPIO_PIN_9
+#define pin_lcd_RS		GPIO_PIN_9
+#define pin_lcd_E		  GPIO_PIN_10
 #define pin_lcd_D4		GPIO_PIN_5
 #define pin_lcd_D5		GPIO_PIN_6
 #define pin_lcd_D6		GPIO_PIN_7
